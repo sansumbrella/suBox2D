8ef9a727dda648871cf9628b21feb8265e7cefc6
David Wicks <wicks.db@gmail.com>
2013-06-16 21:00:10 -0400

    Added license to file and random balls at start.

--- samples/Sandbox/src/SandboxApp.cpp
+++ samples/Sandbox/src/SandboxApp.cpp
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2013 David Wicks
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Rand.h"
@@ -47,12 +74,15 @@ void SandboxApp::setup()
     mFont = Font( "Helvetica", 18.0f );
   }
   createCrazyShape();
+  for( int i = 0; i < 100; ++i )
+  {
+    addBox( Vec2f{Rand::randFloat(getWindowWidth()), Rand::randFloat(getWindowHeight())} );
+  }
 }
 
 void SandboxApp::keyDown(KeyEvent event)
 {
-#define CINDER_IOS true
-#ifdef CINDER_IOS
+#ifdef CINDER_GLES
 #else
   switch ( event.getCode() )
   {
@@ -141,11 +171,6 @@ void SandboxApp::draw()
 	gl::clear( Color::black() );
 
   mSandbox.debugDraw();
-
-  gl::enableAlphaBlending();
-  gl::drawString( "Framerate: " + to_string(getAverageFps()), Vec2f( 10.0f, 10.0f ), Color::white(), mFont );
-	gl::drawString( "Num bodies: " + to_string(mSandbox.getBodyCount() ), Vec2f( 10.0f, 30.0f ), Color::white(), mFont );
-	gl::drawString( "Num contacts: " + to_string(mSandbox.getContactCount() ), Vec2f( 10.0f, 50.0f ), Color::white(), mFont );
 }
 
 CINDER_APP_NATIVE( SandboxApp, RendererGl( RendererGl::AA_NONE ) )
